8a8ce0c1961b9845eb9d3454adb3c7bdf0a5264e
jnerfan <j.n.erfan420@gmail.com>
2021-12-15 21:00:06 +0600

    Contract Complete

--- src/Doshomic_Shop/Pages/Blog/Blog.js
+++ src/Doshomic_Shop/Pages/Blog/Blog.js
@@ -1,46 +1,13 @@
 import React from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Link from "@mui/material/Link";
+import { Box, Typography } from "@mui/material";
 import OurBlog from "../Home/OurBlog/OurBlog";
+import Banner from "../SharedItem/Banner";
 
 const Blog = () => {
   return (
     <Box>
-      <Box className="BannerContainer">
-        <Box
-          className="BannerContainer2"
-          style={{
-            display: "flex",
-            justifyContent: "start",
-            alignItems: "end",
-          }}
-        >
-          <Box sx={{ ml: "50px", mb: "50px" }}>
-            <Typography variant="h1" sx={{ color: "#fff", pb: "10px" }}>
-              Blog
-            </Typography>
-            <Breadcrumbs
-              aria-label="breadcrumb"
-              style={{ color: "#fff", fontSize: "25px" }}
-            >
-              <Link
-                underline="hover"
-                color="inherit"
-                href="/home"
-                style={{ color: "#fff", cursor: "pointer" }}
-              >
-                Home
-              </Link>
-              <Typography
-                color="text.primary"
-                style={{ color: "#fff", fontSize: "25px" }}
-              >
-                Blog
-              </Typography>
-            </Breadcrumbs>
-          </Box>
-        </Box>
+      <Box>
+        <Banner pageName={"Blog"} from={"Home"} />
       </Box>
       <Box sx={{ textAlign: "center", my: "50px" }}>
         <Typography
--- src/Doshomic_Shop/Pages/Contract/Contract.js
+++ src/Doshomic_Shop/Pages/Contract/Contract.js
@@ -1,7 +1,45 @@
 import React from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import "./Contract.css";
 import Banner from "../SharedItem/Banner";
+import MapIcon from "@mui/icons-material/Map";
+import PhoneEnabledIcon from "@mui/icons-material/PhoneEnabled";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import { styled } from "@mui/system";
+import SendIcon from "@mui/icons-material/Send";
+
+const StyledInputElement = styled("input")`
+  width: 100%;
+  font-size: 1.2rem;
+  line-height: 1.4375em;
+  background: #fff;
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 6px 10px;
+  color: #006ef2;
+  transition: width 300ms ease;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const contacts = [
+  {
+    Icon: MapIcon,
+    title: "Office Address",
+    info: "Netrokona, Dhaka, Bangladesh",
+  },
+  {
+    Icon: PhoneEnabledIcon,
+    title: "Phone Number",
+    info: "+8801978352135",
+  },
+  {
+    Icon: MailOutlineIcon,
+    title: "Email Address",
+    info: "j.n.erfan420@gmail.com",
+  },
+];
 
 const Contract = () => {
   return (
@@ -9,6 +47,123 @@ const Contract = () => {
       <Box>
         <Banner pageName={"Contract"} from={"Home"} />
       </Box>
+      <Container sx={{ my: "150px" }}>
+        <Grid container spacing={3}>
+          {contacts.map(({ Icon, title, info }) => (
+            <Grid
+              item
+              xs={12}
+              md={6}
+              lg={4}
+              key={info}
+              sx={{ textAlign: "center" }}
+              className="contractPaper"
+            >
+              <Box
+                sx={{
+                  backgroundColor: "#0056FB",
+                  px: "50px",
+                  py: "40px",
+                  borderRadius: "10px",
+                }}
+              >
+                <Box sx={{ mt: "-90px", mb: "50px" }}>
+                  <Icon
+                    sx={{
+                      fontSize: "50px",
+                      color: "#0056FB",
+                      backgroundColor: "#fff",
+                      borderRadius: "100%",
+                      height: "100px",
+                      width: "100px",
+                      padding: "20px",
+                    }}
+                  />
+                </Box>
+                <Box>
+                  <h4
+                    style={{
+                      fontWeight: 500,
+                      marginTop: "20px",
+                      color: "#fff",
+                    }}
+                  >
+                    {title}
+                  </h4>
+                  <p
+                    style={{
+                      fontWeight: 500,
+                      marginTop: "20px",
+                      color: "#fff",
+                    }}
+                  >
+                    {info}
+                  </p>
+                </Box>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+
+        <Box sx={{ my: "100px" }}>
+          <Typography
+            variant="h4"
+            sx={{
+              color: "#000",
+              fontWeight: "bold",
+              borderLeft: "5px solid #006EF2",
+              mb: "50px",
+              mt: "50px",
+            }}
+          >
+            <span style={{ marginLeft: "15px" }}>CONTRACT FORM</span>
+          </Typography>
+          <Box
+            sx={{
+              boxShadow: "1px 1px 15px #9e9e9e",
+              p: "50px",
+              borderRadius: "12px",
+            }}
+          >
+            <form>
+              <StyledInputElement
+                sx={{ my: "10px" }}
+                placeholder="Enter Name"
+                required
+                type="name"
+                name="name"
+              />
+              <StyledInputElement
+                sx={{ my: "10px" }}
+                placeholder="Enter Email Address"
+                required
+                type="email"
+                name="user_email"
+              />
+              <StyledInputElement
+                sx={{ my: "10px", pb: "100px" }}
+                placeholder="Enter Message"
+                required
+                type="text"
+                name="message"
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                endIcon={<SendIcon />}
+                sx={{
+                  backgroundColor: "#474747",
+                  color: "#fff",
+                  borderRadius: "10px",
+                  mt: "10px",
+                }}
+              >
+                Send
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Container>
     </Box>
   );
 };
