ecb5cd7567c5fe9a200b21b0b3740c5881ff68ea
jnErfan <j.n.erfan420@gmail.com>
2022-01-26 01:18:56 +0600

    Membership Add Complete

--- src/Doshomic_Shop/Pages/DashboardComponent/AddMemberShips/AddMemberShips.js
+++ src/Doshomic_Shop/Pages/DashboardComponent/AddMemberShips/AddMemberShips.js
@@ -1,7 +1,10 @@
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 import { Button, Typography } from "@mui/material";
 import { styled } from "@mui/system";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const AddMemberShips = () => {
   const StyledInputElement = styled("input")`
@@ -13,8 +16,49 @@ const AddMemberShips = () => {
       transform: scale(1.03);
     }
   `;
+
+  const { register, handleSubmit, reset } = useForm();
+  const [isLoading, setIsLoading] = useState(false);
+  const onSubmit = (data) => {
+    setIsLoading(true);
+    axios.post("http://localhost:5000/addMembership", data).then((result) => {
+      if (result.data.insertedId) {
+        setTimeout(() => {
+          setIsLoading(false);
+          Swal.fire({
+            icon: "success",
+            title: "Membership Add Successful",
+            showConfirmButton: false,
+            timer: 3000,
+          });
+          reset();
+        }, 3000);
+      } else {
+        setIsLoading(false);
+      }
+    });
+  };
+
   return (
     <Box sx={{ my: "100px" }}>
+      {isLoading && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            style={{
+              position: "fixed",
+              zIndex: "99999",
+              marginTop: "-120px",
+            }}
+            src="https://i.pinimg.com/originals/ca/a3/df/caa3df6a361bd55a3412cb60bb14aa7d.gif"
+            alt=""
+          />
+        </Box>
+      )}
       <Box sx={{ textAlign: "center" }}>
         <Typography variant="h3" sx={{ fontWeight: "bold", color: "#0C163C" }}>
           Create Memberships
@@ -45,8 +89,9 @@ const AddMemberShips = () => {
             mt: "10px",
           }}
         >
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <StyledInputElement
+              {...register("service")}
               placeholder="Membership Name"
               multiline
               rows={4}
@@ -61,6 +106,7 @@ const AddMemberShips = () => {
             />
             <br />
             <StyledInputElement
+              {...register("price")}
               placeholder="Price"
               multiline
               type="number"
@@ -75,6 +121,7 @@ const AddMemberShips = () => {
             />
             <br />
             <StyledInputElement
+              {...register("limit")}
               placeholder="Limit"
               multiline
               rows={4}
@@ -89,6 +136,7 @@ const AddMemberShips = () => {
             />
             <br />
             <StyledInputElement
+              {...register("bonus")}
               placeholder="Referral Bonus"
               multiline
               rows={4}
@@ -103,6 +151,7 @@ const AddMemberShips = () => {
             />
             <br />
             <StyledInputElement
+              {...register("validity")}
               placeholder="Validity"
               multiline
               rows={4}
